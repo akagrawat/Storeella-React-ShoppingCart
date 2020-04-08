9d757bed9887f983a29d6a35a9b03d00d72c00a1
Ashish Agrawat <akagrawat@gmail.com>
2020-04-08 15:45:16 +0530

    Adding sign in component
    Signed-off-by: Ashish Agrawat <akagrawat@gmail.com>

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,8 +1,47 @@
-import React from 'react';
-import './sign-in.scss';
+import React, { useState } from 'react';
+import FormInput from '../../components/form-input/form-input.component';
+import CustomButton from '../../components/custom-button/custom-button.component';
+import './sign-in.style.scss';
+
 
 const SinIn = () => {
-    return ()
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(email);
+        console.log(password);
+        setEmail('');
+        setPassword('');
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        switch (name) {
+            case 'email':
+                setEmail(value);
+                break;
+            case 'password':
+                setPassword(value);
+                break;
+            default:
+                break;
+        }
+    }
+
+    return (
+        <div className="sign-in">
+            <h2>I already have an acoount</h2>
+            <span>Sign with your email and passsword</span>
+            <form onSubmit={handleSubmit}>
+                <FormInput type="email" onChange={handleChange} name="email" value={email} label="Email" required />
+
+                <FormInput type="password" onChange={handleChange} name="password" value={password} label="Password" required />
+                <CustomButton type="submit" > Sign In </CustomButton>
+            </form>
+        </div>
+    )
 }
 
 export default SinIn;
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './sign-in-and-sign-up.style.scss';
-
+import SignIn from '../../components/sign-in/sign-in.component';
 const SignInAndSignUp = () => {
 
     return (
-        <div className="sign-in-and-sign-up">SIGN IN</div>
+        <div className="sign-in-and-sign-up">
+            <SignIn />
+        </div>
     )
 }
 
