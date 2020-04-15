77c3726c8e8b5ac90c4ed45c00ddff5b160b0ce1
Ashish Agrawat <akagrawat@gmail.com>
2020-04-15 15:09:01 +0530

    Added redux-persist to store data in localstorage and implemented remove item form checkout page functionality
    Signed-off-by: Ashish Agrawat <akagrawat@gmail.com>

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './root-reducer';
 import { logger } from 'redux-logger';
+import { persistStore } from 'redux-persist';
 
 const middelwares = [logger];
 
-const Store = createStore(rootReducer, applyMiddleware(...middelwares));
+export const store = createStore(rootReducer, applyMiddleware(...middelwares));
 
-export default Store;
+export const persistor = persistStore(store)
+
+export default { store, persistor };
